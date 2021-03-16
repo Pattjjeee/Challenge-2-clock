1e6c300f1b38ddd0de8d50b19e37eb688fa6c7a7
Pattjjeee <79310750+Pattjjeee@users.noreply.github.com>
2021-03-16 13:53:52 +0100

    Initial commit

---
+++ scripts/script.js
@@ -0,0 +1,74 @@
+var today = new Date();
+
+document.getElementById('date').innerHTML = today.getDate() + '/' + (today.getMonth()+1);
+
+var maanden = new Array('januari', 'februari', 'maart', 'april', 'mei', 'juni', 'juli', 'augustus', 'september', 'oktober', 'november', 'december');
+document.getElementById('date').innerHTML = today.getDate() + ' ' + maanden[today.getMonth()];
+
+var dagen = new Array('zondag', 'maandag', 'dinsdag', 'woensdag', 'donderdag', 'vrijdag', 'zaterdag');
+
+document.getElementById('dag').innerHTML = dagen[today.getDay()];
+
+
+
+function klok() {
+	var today = new Date();
+	var seconds = today.getSeconds();
+	var minutes = today.getMinutes();
+	var hour = today.getHours();
+
+// verandert de visualisatie van de tijd op een dag
+
+	if (hour > 5 && hour <= 11) {
+		document.getElementById('body').className = 'ochtend';
+	} else if (hour > 11 && hour <= 17) {
+		document.getElementById('body').className = 'middag';
+	} else if (hour > 17 && hour <= 24) {
+		document.getElementById('body').className = 'avond';
+	} else {
+		document.getElementById('body').className = 'nacht';
+	}
+
+// animeert de klok op de hele en halve minuten
+
+	if (seconds % 60 == 0) {
+		document.getElementById('clock').classList.add('animate');
+	} else if (seconds % 60 >= 3) {
+		document.getElementById('clock').classList.remove('animate');
+	}
+
+	if (seconds % 30 == 0) {
+		document.getElementById('clock').classList.add('animate');
+	} else if (seconds % 30 >= 3) {
+		document.getElementById('clock').classList.remove('animate');
+	}
+
+// Voegt een 0 toe 
+
+	if (seconds < 10) {
+		seconds = '0' + seconds;
+	}
+	if (minutes < 10) {
+		minutes = '0' + minutes;
+	}
+	if (hour < 10) {
+		hour = '0' + hour;
+	}
+
+	document.getElementById('clock').innerHTML = hour + ':' + minutes + ':' + seconds;
+
+}
+
+// Laat de klok iedere seconde verversen
+
+klok();
+setInterval(klok, 1000);
+
+
+
+
+
+
+
+
+
